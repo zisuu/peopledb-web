9a2c4b5e52406e52ab56c06c8ee4eb668f53b9c2
zisuu <david.ziswiler@finecloud.ch>
2022-09-21 09:16:13 +0200

    add bootstrap

--- src/main/java/ch/finecloud/peopledbweb/web/controller/PeopleController.java
+++ src/main/java/ch/finecloud/peopledbweb/web/controller/PeopleController.java
@@ -20,6 +20,8 @@ public class PeopleController {
                 new Person(10l, "Jake", "Snake", LocalDate.of(1950, 1, 8), new BigDecimal(50000)),
                 new Person(20l, "Sara", "Smith", LocalDate.of(1960, 2, 7), new BigDecimal(60000)),
                 new Person(30l, "Johnny", "Jackson", LocalDate.of(1970, 3, 6), new BigDecimal(70000)),
+                new Person(31l, "Jerry", "Norris", LocalDate.of(1971, 3, 6), new BigDecimal(70000)),
+                new Person(32l, "Smith", "Jane", LocalDate.of(1972, 3, 6), new BigDecimal(70000)),
                 new Person(40l, "Bonny", "Norris", LocalDate.of(1980, 4, 5), new BigDecimal(80000))
         );
         model.addAttribute("people", people);
