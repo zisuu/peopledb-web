efdfc75332ebffa4863a5ed192e8f47717c6e5dd
zisuu <david.ziswiler@finecloud.ch>
2022-09-25 14:21:21 +0200

    add delete function

--- src/main/java/ch/finecloud/peopledbweb/biz/model/Person.java
+++ src/main/java/ch/finecloud/peopledbweb/biz/model/Person.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -18,9 +19,23 @@ public class Person {
     @Id
     @GeneratedValue
     private Long id;
+
+    @NotEmpty(message = "First name can not be empty")
     private String firstName;
+
+    @NotEmpty(message = "Last name can not be empty")
     private String lastName;
+
+    @Past(message = "Date of Birth must be in the Past")
+    @NotNull(message = "Date of Birth can not be empty")
     private LocalDate dob;
+
+    @Email(message = "Email must be valid")
+    @NotEmpty(message = "Email can not be empty")
+    private String email;
+
+    @DecimalMin(value = "1000.00", message = "Salary must be at least 1000.00")
+    @NotNull(message = "salary can not be empty")
     private BigDecimal salary;
 }
 
--- src/main/java/ch/finecloud/peopledbweb/web/controller/PeopleController.java
+++ src/main/java/ch/finecloud/peopledbweb/web/controller/PeopleController.java
@@ -3,9 +3,12 @@ package ch.finecloud.peopledbweb.web.controller;
 import ch.finecloud.peopledbweb.biz.model.Person;
 import ch.finecloud.peopledbweb.data.PersonRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/people")
@@ -21,8 +24,32 @@ public class PeopleController {
     public Iterable<Person> getPeople() {
         return personRepository.findAll();
     }
+
+    @ModelAttribute
+    public Person getPerson() {
+        return new Person();
+    }
     @GetMapping
     public String showPeoplePage() {
         return "people";
+    }
+
+    @PostMapping
+    public String savePerson(@Valid Person person, Errors errors) {
+        System.out.println(person);
+        if (!errors.hasErrors()) {
+            personRepository.save(person);
+            return "redirect:people";
+        }
+        return "people";
+    }
+
+    @PostMapping(params = "delete=true")
+    public String deletePeople(@RequestParam Optional<List<Long>> selections) {
+        System.out.println(selections);
+        if (selections.isPresent()) {
+            personRepository.deleteAllById(selections.get());
+        }
+        return "redirect:people";
     }
 }
